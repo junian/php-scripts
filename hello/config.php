e9068bd28040749b266781266312c950001b3853
junian <junian.triajianto@gmail.com>
2013-07-29 22:05:12 -0400

    initial php server from this awesome tutorial http://www.androidhive.info/2012/10/android-push-notifications-using-google-cloud-messaging-gcm-php-and-mysql/

---
+++ hello/config.php
@@ -0,0 +1,8 @@
+<?php
+define("DB_HOST", "localhost");
+define("DB_USER", "junian");
+define("DB_PASSWORD", "");
+define("DB_DATABASE", "c9");
+
+define("GOOGLE_API_KEY", "AIzaSyAmP53CC_5guKObEg2Dkubp-5edrMerzCo");
+?>
